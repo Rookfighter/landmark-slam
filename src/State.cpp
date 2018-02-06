a80cfaba3cd2c081b19fdf10c46edfba25c71444
Fabian Meyer <user8324@posteo.net>
2018-02-06 13:22:03 +0100

    Add State module

---
+++ src/State.cpp
@@ -0,0 +1,70 @@
+/*
+ * State.cpp
+ *
+ *  Created on: 06 Feb 2018
+ *      Author: Fabian Meyer
+ */
+
+#include "State.hpp"
+
+namespace slam
+{
+    StatePose State::getPose() const
+    {
+        StatePose result;
+
+        result.pose << mean(0),
+                       mean(1),
+                       mean(2);
+
+        result.cov << cov(0,0), cov(0,1), cov(0,2),
+                      cov(1,0), cov(1,1), cov(1,2),
+                      cov(2,0), cov(2,1), cov(2,2);
+
+        return result;
+    }
+
+    void State::setPose(const StatePose &p)
+    {
+        mean(0) = p.pose(0);
+        mean(1) = p.pose(1);
+        mean(2) = p.pose(2);
+
+        for(size_t i = 0; i < 3; ++i)
+            for(size_t j = 0; j < 3; ++j)
+                cov(i,j) = p.cov(i,j);
+    }
+
+    StateLandmark State::getLandmark(const size_t c) const
+    {
+        assert((3 + 2*c) + 1 < mean.rows());
+
+        StateLandmark result;
+        size_t idx = 3 + c*2;
+
+        result.pos << mean(idx),
+                      mean(idx+1);
+
+        result.cov << cov(idx, idx),  cov(idx, idx+1),
+                      cov(idx+1,idx), cov(idx+1,idx+1);
+
+        result.c = c;
+
+        return result;
+    }
+
+    void State::setLandmark(const StateLandmark &lm)
+    {
+        assert((3 + 2*lm.c) + 1 < mean.rows());
+
+        size_t idx = 3 + 2*lm.c;
+
+        mean(idx)   = lm.pos(0);
+        mean(idx+1) = lm.pos(1);
+
+        cov(idx, idx)     = lm.cov(0,0);
+        cov(idx, idx+1)   = lm.cov(0,1);
+        cov(idx+1, idx)   = lm.cov(1,0);
+        cov(idx+1, idx+1) = lm.cov(1,1);
+    }
+}
---
+++ src/State.hpp
@@ -0,0 +1,47 @@
+/*
+ * State.hpp
+ *
+ *  Created on: 06 Feb 2018
+ *      Author: Fabian Meyer
+ */
+
+ #ifndef SLAM_STATE_HPP_
+ #define SLAM_STATE_HPP_
+
+#include "Data.hpp"
+
+namespace slam
+{
+    typedef Eigen::VectorXd StateMean;
+    typedef Eigen::MatrixXd StateCov;
+
+    struct StatePose
+    {
+        Pose pose;
+        PoseCov cov;
+    };
+
+    struct StateLandmark
+    {
+        Position pos;
+        PositionCov cov;
+        size_t c;
+    };
+
+    class State
+    {
+    public:
+        StateMean mean;
+        StateCov cov;
+
+        StatePose getPose() const;
+        void setPose(const StatePose &p);
+
+        StateLandmark getLandmark(const size_t c) const;
+        void setLandmark(const StateLandmark &lm);
+    };
+
+
+}
+
+#endif
